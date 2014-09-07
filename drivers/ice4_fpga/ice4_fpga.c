a5b0799788af1705e35ffdfa6c7900cdbb910b58
UpInTheAir <tyson_cj@hotmail.com>
2014-09-08 06:37:23 +0700

    config: disable DEFERRED_INITCALLS for compile

--- drivers/ice4_fpga/ice4_fpga.c
+++ drivers/ice4_fpga/ice4_fpga.c
@@ -1065,11 +1065,11 @@ err_create_wq:
 #endif
 	return 0;
 }
-#if defined(CONFIG_ICE4_TWO_FUNC_INPUT)
-deferred_module_init(barcode_emul_init);
-#else
+// #if defined(CONFIG_ICE4_TWO_FUNC_INPUT) /* Disabled here & defconfig to compile CM Port kernel */
+// deferred_module_init(barcode_emul_init);
+// #else
 late_initcall(barcode_emul_init);
-#endif
+// #endif
 
 static void __exit barcode_emul_exit(void)
 {
